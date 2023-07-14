bec975c0b3160a1c78e1665d0382ed971dc85a4c
SOCIALSCIENCEai <nick.sh.oh@socialscience.ai>
2023-07-14 02:27:04 +0100

    lex_dict to self.lex_dict in CompliSent.py

--- socscikit/CompliSent.py
+++ socscikit/CompliSent.py
@@ -4,17 +4,18 @@ import pickle
 class lexicon:
     def __init__(self): 
         self.script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.lex_dict = None 
     
     def dictionary(self, idx:str):
         
         if idx == 'MASTER_v2022':   
             file_path = os.path.join(self.script_dir, 'lexicon_dictionary', 'MASTER', 'MASTER_v2022.pickle')           
             with open(file_path, 'rb') as handle: 
-                lex_dict = pickle.load(handle)
+                self.lex_dict = pickle.load(handle)
         
         elif idx == "VADER": 
             file_path = os.path.join(self.script_dir, 'lexicon_dictionary', 'VADER', 'VADER.pickle')
             with open(file_path, 'rb') as handle: 
-                lex_dict = pickle.load(handle)
+                self.lex_dict = pickle.load(handle)
                 
-        return lex_dict
+        return self.lex_dict
