8b1eebfdd9443ec155d41a8bb8b714c9378bb77c
SOCIALSCIENCEai <nick.sh.oh@socialscience.ai>
2023-07-15 23:45:55 +0100

    utils.py added

--- socscikit/CompliSent.py
+++ socscikit/CompliSent.py
@@ -1,12 +1,13 @@
 import os 
 import pickle 
+from socscikit.utils import CS 
 
 class lexicon:
     def __init__(self): 
         self.script_dir = os.path.dirname(os.path.abspath(__file__))
         self.lex_dict = None 
     
-    def dictionary(self, idx:str):
+    def load_dict(self, idx:str):
         
         if idx == 'MASTER_v2022':   
             file_path = os.path.join(self.script_dir, 'lexicon_dictionary', 'MASTER', 'MASTER_v2022.pickle')           
@@ -19,3 +20,7 @@ class lexicon:
                 self.lex_dict = pickle.load(handle)
                 
         return self.lex_dict
+    
+    def overview(self, dictionary:dict): 
+        #dict:str = Consider if users simply come up with the lex_dict_idx
+        return CS.summarise_lex_dict(dictionary)
