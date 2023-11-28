9989d8a1fa90bf0f66519e94ef70e1b4a28f8a85
SOCIALSCIENCEai <nick.sh.oh@socialscience.ai>
2023-11-28 00:57:33 +0000

    clean dir struc

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'socscikit',         # How you named your package folder (MyLib)
   packages = ['socscikit'],   
-  version = '0.0.6.0',      
+  version = '0.0.6.1',      
   license='Apache-2.0',        
   description = 'TYPE YOUR DESCRIPTION HERE',   
   author = 'Nick S.H Oh',                   #
   author_email = 'nick.sh.oh@socialscience.ai',      
   url = 'https://github.com/socius-org/socscikit',  
-  download_url = 'https://github.com/socius-org/socscikit/archive/refs/tags/0.0.6.tar.gz', 
+  download_url = 'https://github.com/socius-org/socscikit/archive/refs/tags/0.0.6.1.tar.gz', 
   keywords = ['AI', 'SOCIAL SCIENCE'],   # Keywords that define your package best
   install_requires=[            
           'torch',
